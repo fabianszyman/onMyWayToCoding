deaa5787ed248e39f8a162dd7687f55ff6cdd8f9
Fabian Szyman <fabianszyman@gmail.com>
2022-09-06 14:19:09 +0200

    add *submitToDoHandler* function

--- react-tutorials/react-to-do-app/src/App.js
+++ react-tutorials/react-to-do-app/src/App.js
@@ -1,15 +1,23 @@
+import React, {useState} from 'react';
 import './App.css';
 // importing components
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState(""); 
+  const [todos, setToDos] = useState([]);
   return (
     <div className="App">
       <header>
-        <h1>Fabian's To Do List</h1>
+  <h1>Fabian's To Do List</h1>
       </header>
-      <Form />
+      <Form 
+      inputText={inputText}
+      todos={todos} 
+      setToDos={setToDos} 
+      setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
