25830aa7729a1a6c10161e7ef51da9d32743dbe3
Fabian Szyman <fabianszyman@gmail.com>
2022-09-24 21:20:07 +0200

    adding getLocalToDos() function

--- react-tutorials/react-to-do-app/src/App.js
+++ react-tutorials/react-to-do-app/src/App.js
@@ -5,10 +5,16 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  //State stuff
   const [inputText, setInputText] = useState(""); 
   const [todos, setToDos] = useState([]);
   const [status, setStatus] = useState("all");
   const [filteredToDos, setFilteredToDos] = useState([]);
+
+  //RUN ONCE when the app start
+  useEffect(() => {
+    getLocalToDos();
+  }, []);
 
   // USE EFFECT 
   useEffect(() => {
@@ -31,13 +37,20 @@ function App() {
     }
   }
 
-  //Save to Local
-  const saveLocalToDos = () => {
-    if(localStorage.getItem("todos") === null){
-      localStorage.setItem("todos", JSON.stringify([]));
+  const getLocalToDos = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
     }
     else{
-      localStorage.setItem("todos", JSON.stringify(todos));
+      var todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setToDos(todoLocal)
+    }
+  };
+  
+  //Save to Local
+  const saveLocalToDos = () => {
+    if (todos.length > 0) { 
+      localStorage.setItem('todos', JSON.stringify(todos)) 
     }
   };
 
--- react-tutorials/react-to-do-app/src/components/Form.js
+++ react-tutorials/react-to-do-app/src/components/Form.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Form = ({setInputText, inputText, todos, setToDos, setStatus}) => {
     // Here I can write javascript code and function
     const inputTextHandler = (e) => {
-        console.log(e.target.value);
         setInputText(e.target.value);
     };
     const submitToDoHandler = (e) => {
